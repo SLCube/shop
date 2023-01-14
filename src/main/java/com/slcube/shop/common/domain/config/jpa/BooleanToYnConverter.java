02f000318652d99992264183f3f8c41770032c04
SLCube <kgm0096@naver.com>
2023-01-14 20:08:37 +0900

    chore : boolean converter 추가

---
+++ src/main/java/com/slcube/shop/common/domain/config/jpa/BooleanToYnConverter.java
@@ -0,0 +1,17 @@
+package com.slcube.shop.common.domain.config.jpa;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class BooleanToYnConverter implements AttributeConverter<Boolean, String> {
+    @Override
+    public String convertToDatabaseColumn(Boolean attribute) {
+        return (attribute != null && attribute) ? "Y" : "N";
+    }
+
+    @Override
+    public Boolean convertToEntityAttribute(String dbData) {
+        return "Y".equalsIgnoreCase(dbData);
+    }
+}
