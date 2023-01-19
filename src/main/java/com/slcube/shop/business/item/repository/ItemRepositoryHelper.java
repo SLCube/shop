c8af3a46b6f10da327a24866f94c59eaa20135b8
SLCube <kgm0096@naver.com>
2023-01-19 20:58:59 +0900

    refactor : 필요없어진 주석 제거

--- src/main/java/com/slcube/shop/business/item/repository/ItemRepositoryHelper.java
+++ src/main/java/com/slcube/shop/business/item/repository/ItemRepositoryHelper.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class ItemRepositoryHelper {
 
-    // helper class도 반복되고 있다.
     public Item findByNotDeleted(ItemRepository itemRepository, Long itemId) {
         return itemRepository.findByNotDeleted(itemId)
                 .orElseThrow(() -> new IllegalArgumentException("상품 정보가 없습니다. id = " + itemId));
--- src/main/java/com/slcube/shop/business/review/repository/ReviewRepositoryHelper.java
+++ src/main/java/com/slcube/shop/business/review/repository/ReviewRepositoryHelper.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class ReviewRepositoryHelper {
 
-    // ItemRepositoryHelper 가 반복되는거처럼 ReviewRepositoryHelper도 반복되고있다.
     public Review findByNotDeleted(ReviewRepository reviewRepository, Long reviewId) {
         return reviewRepository.findByNotDeleted(reviewId)
                 .orElseThrow(() -> new IllegalArgumentException("리뷰 정보를 찾을 수 없습니다. id = " + reviewId));
