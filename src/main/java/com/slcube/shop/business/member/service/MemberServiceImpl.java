ba5455d1a91080eec970eae75460165461fca1ce
SLCube <kgm0096@naver.com>
2023-01-21 19:05:21 +0900

    feat: 비밀번호 암호화적용

--- src/main/java/com/slcube/shop/business/member/service/MemberServiceImpl.java
+++ src/main/java/com/slcube/shop/business/member/service/MemberServiceImpl.java
@@ -4,18 +4,26 @@ import com.slcube.shop.business.member.domain.Member;
 import com.slcube.shop.business.member.dto.*;
 import com.slcube.shop.business.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public Long signUp(MemberSignUpRequestDto requestDto) {
+        String password = requestDto.getPassword();
+        requestDto.setPassword(passwordEncoder.encode(password));
+
+        log.debug("password : {}", requestDto.getPassword());
         Member member = MemberMapper.toEntity(requestDto);
         return memberRepository.save(member).getId();
     }
@@ -35,7 +43,7 @@ public class MemberServiceImpl implements MemberService {
         Member member = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException("아이디 혹은 비밀번호를 확인해주세요."));
 
-        if (password.equals(member.getPassword())) {
+        if (!passwordEncoder.matches(password, member.getPassword())) {
             throw new IllegalArgumentException("아이디 혹은 비밀번호를 확인해주세요.");
         }
 
@@ -47,7 +55,8 @@ public class MemberServiceImpl implements MemberService {
         Member member = memberRepository.findByEmail(requestDto.getEmail())
                 .orElseThrow(() -> new IllegalArgumentException("회원 정보를 찾을 수 없습니다."));
 
-        member.changePassword(requestDto.getChangedPassword());
+        String changedPassword = requestDto.getChangedPassword();
+        member.changePassword(passwordEncoder.encode(changedPassword));
 
         return member.getId();
     }
