9f8e054b2955d9cd9ec52cc6fe486000b75e0c60
SLCube <kgm0096@naver.com>
2023-01-24 21:33:00 +0900

    feat: default exception logging 추가

--- src/main/java/com/slcube/shop/common/exception/CustomExceptionHandler.java
+++ src/main/java/com/slcube/shop/common/exception/CustomExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.slcube.shop.common.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@Slf4j
 @RestControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
@@ -15,6 +17,7 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {Exception.class})
     protected ResponseEntity handleException(Exception e) {
+        log.error("internal server error", e);
         return CustomErrorResponse.toResponseEntity(CustomErrorCode.INTERNAL_SERVER_ERROR);
     }
 }
