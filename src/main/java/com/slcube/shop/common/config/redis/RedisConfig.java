54abccf89e425a79044c06676d3c3101e00850bc
SLCube <kgm0096@naver.com>
2023-01-29 20:05:44 +0900

    chore: redis 설정 추가

---
+++ src/main/java/com/slcube/shop/common/config/redis/RedisConfig.java
@@ -0,0 +1,22 @@
+package com.slcube.shop.common.config.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@EnableRedisHttpSession
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+}
