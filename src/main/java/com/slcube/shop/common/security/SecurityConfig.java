36e144bc75ac5831e2408113d3d063a14e628ea3
SLCube <kgm0096@naver.com>
2023-01-30 21:31:48 +0900

    feat: CustomAuthenticationFailure 구현

--- src/main/java/com/slcube/shop/common/security/SecurityConfig.java
+++ src/main/java/com/slcube/shop/common/security/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.slcube.shop.common.security;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,6 +17,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     @Bean
---
+++ src/main/java/com/slcube/shop/common/security/handler/CustomAuthenticationFailure.java
@@ -0,0 +1,43 @@
+package com.slcube.shop.common.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.slcube.shop.common.exception.CustomErrorCode.*;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.MediaType.*;
+
+@Component
+public class CustomAuthenticationFailure implements AuthenticationFailureHandler {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+
+        String errorMessage = INVALID_EMAIL_OR_PASSWORD.getErrorMessage();
+
+        response.setStatus(UNAUTHORIZED.value());
+        response.setContentType(APPLICATION_JSON_VALUE);
+
+        if (exception instanceof BadCredentialsException) {
+            errorMessage = INVALID_EMAIL_OR_PASSWORD.getErrorMessage();
+        } else if (exception instanceof DisabledException) {
+            errorMessage = DISABLED_MEMBER.getErrorMessage();
+        } else if (exception instanceof CredentialsExpiredException) {
+            errorMessage = SESSION_EXPIRED.getErrorMessage();
+        }
+
+        objectMapper.writeValue(response.getWriter(), errorMessage);
+    }
+}
