a3b115dd2dce4a6ea2fc6577be412078d60a7a7b
SLCube <kgm0096@naver.com>
2023-02-02 20:54:00 +0900

    fix: macOs & Linux에서 EmbeddedRedis 실행시 포트충돌나는 현상 해결

--- src/main/java/com/slcube/shop/common/config/redis/EmbeddedRedisConfig.java
+++ src/main/java/com/slcube/shop/common/config/redis/EmbeddedRedisConfig.java
@@ -2,10 +2,14 @@ package com.slcube.shop.common.config.redis;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import redis.embedded.RedisServer;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @Component
 public class EmbeddedRedisConfig {
@@ -16,8 +20,8 @@ public class EmbeddedRedisConfig {
     private RedisServer redisServer;
 
     @PostConstruct
-    public void startRedis() {
-        redisServer = new RedisServer(port);
+    public void startRedis() throws IOException {
+        redisServer = new RedisServer(isRedisRunning() ? findAvailablePort() : port);
         redisServer.start();
     }
 
@@ -26,5 +30,42 @@ public class EmbeddedRedisConfig {
         if (redisServer.isActive()) {
             redisServer.stop();
         }
+    }
+
+    private boolean isRedisRunning() throws IOException {
+        return isRunning(excuteGrepProcess(port));
+    }
+
+    public int findAvailablePort() throws IOException {
+        for (int port = 10000; port <= 65535; port++) {
+            Process process = excuteGrepProcess(port);
+
+            if (!isRunning(process)) {
+                return port;
+            }
+        }
+
+        throw new IllegalArgumentException("Not Found Available Port : 10000 ~ 65535");
+    }
+
+    private Process excuteGrepProcess(int port) throws IOException {
+        String command = String.format("netstat -nat | grep LISTEN|grep %d", port);
+        String[] shell = {"/bin/sh", "-c", command};
+        return Runtime.getRuntime().exec(shell);
+    }
+
+    private boolean isRunning(Process process) {
+        String line;
+        StringBuilder pidInfo = new StringBuilder();
+
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            while ((line = input.readLine()) != null) {
+                pidInfo.append(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return StringUtils.hasText(pidInfo.toString());
     }
 }
