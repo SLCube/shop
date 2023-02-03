a2dc891c1f575a0b4391436d6c636c859334f76c
SLCube <kgm0096@naver.com>
2023-02-03 21:51:21 +0900

    style: 필요없어진 주석 제거

--- src/main/java/com/slcube/shop/business/review/repository/ReportedReviewRepository.java
+++ src/main/java/com/slcube/shop/business/review/repository/ReportedReviewRepository.java
@@ -4,5 +4,4 @@ import com.slcube.shop.business.review.domain.ReportedReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReportedReviewRepository extends JpaRepository<ReportedReview, Long> {
-    // review Entity와 연관성이 있다 생각해서 reportedReview Entity를 하나의 패키지로 묶었는데 뭔가 이상하다.
 }
