6625fe4334c8e40804008e5077a739bdd520ebe3
SLCube <kgm0096@naver.com>
2023-02-07 22:32:47 +0900

    test: 주소저장에 대한 테스트코드 작성

--- src/test/java/com/slcube/shop/business/address/service/AddressServiceTest.java
+++ src/test/java/com/slcube/shop/business/address/service/AddressServiceTest.java
@@ -4,11 +4,14 @@ import com.slcube.shop.business.address.dto.AddressResponseDto;
 import com.slcube.shop.business.address.dto.AddressSaveRequestDto;
 import com.slcube.shop.business.member.domain.Member;
 import com.slcube.shop.business.member.repository.MemberRepository;
+import com.slcube.shop.common.exception.AddressNotRegisterAnymore;
+import com.slcube.shop.common.security.WithMockMember;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -17,25 +20,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@WithMockMember
 class AddressServiceTest {
 
     @Autowired
     private AddressService addressService;
-
     @Autowired
     private MemberRepository memberRepository;
-
-    private Long memberId;
+    private Member member;
 
     @BeforeEach
     void beforeEach() {
-        Member member = Member.builder()
-                .email("test@naver.com")
-                .password("test password")
-                .username("test user name")
-                .build();
-
-        memberId = memberRepository.save(member).getId();
+        member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        memberRepository.save(member);
     }
 
     @Test
@@ -48,9 +45,8 @@ class AddressServiceTest {
         requestDto.setStreet("test street");
         requestDto.setComment("test comment");
 
-        Long saveId = addressService.saveAddress(requestDto, memberId);
-
-        AddressResponseDto findAddress = addressService.findAddress(saveId, memberId);
+        Long saveId = addressService.saveAddress(requestDto, member);
+        AddressResponseDto findAddress = addressService.findAddress(saveId, member);
 
         assertAll(
                 () -> assertThat(findAddress.getCity()).isEqualTo("test city"),
@@ -64,7 +60,23 @@ class AddressServiceTest {
     @Test
     @DisplayName("배송지가 3개일때 배송지 추가 시도")
     void addressNotRegisterAnymoreTest() {
+        for (int i = 0; i < 3; i++) {
+            AddressSaveRequestDto requestDto = new AddressSaveRequestDto();
+            requestDto.setDefaultAddress(false);
+            requestDto.setCity("test city " + i);
+            requestDto.setZipcode("test zipcode " + i);
+            requestDto.setStreet("test street " + i);
+            requestDto.setComment("test comment " + i);
+            addressService.saveAddress(requestDto, member);
+        }
 
+        AddressSaveRequestDto fourthAddress = new AddressSaveRequestDto();
+        fourthAddress.setCity("fourth address city");
+        fourthAddress.setZipcode("fourth address zipcode");
+        fourthAddress.setStreet("fourth address street");
+
+        assertThrows(AddressNotRegisterAnymore.class,
+                () -> addressService.saveAddress(fourthAddress, member));
     }
 
     @Test
