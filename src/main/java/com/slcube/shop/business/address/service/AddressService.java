5641af84dd5ae20a9813a1a0472efc031fcfc920
SLCube <kgm0096@naver.com>
2023-02-10 22:03:12 +0900

    refactor: AddressService interface에 Service annotation 삭제 및 public 접근제어자 제거

--- src/main/java/com/slcube/shop/business/address/service/AddressService.java
+++ src/main/java/com/slcube/shop/business/address/service/AddressService.java
@@ -5,16 +5,14 @@ import com.slcube.shop.business.address.dto.AddressResponseDto;
 import com.slcube.shop.business.address.dto.AddressSaveRequestDto;
 import com.slcube.shop.business.address.dto.AddressUpdateRequestDto;
 import com.slcube.shop.business.member.domain.Member;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public interface AddressService {
 
-    public Long saveAddress(AddressSaveRequestDto requestDto, Member member);
-    public AddressResponseDto findAddress(Long addressId, Member member);
-    public List<AddressListResponseDto> findAllAddresses(Member member);
-    public Long updateAddress(Long addressId, AddressUpdateRequestDto requestDto, Member member);
-    public Long deleteAddress(Long addressId, Member member);
+    Long saveAddress(AddressSaveRequestDto requestDto, Member member);
+    AddressResponseDto findAddress(Long addressId, Member member);
+    List<AddressListResponseDto> findAllAddresses(Member member);
+    Long updateAddress(Long addressId, AddressUpdateRequestDto requestDto, Member member);
+    Long deleteAddress(Long addressId, Member member);
 }
