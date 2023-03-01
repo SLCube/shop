d96741c8b615b8a637ad583e07f8be49f3130b07
SLCube <kgm0096@naver.com>
2023-03-01 17:22:51 +0900

    refactor: MemberSessionDto의 Setter 삭제

--- src/main/java/com/slcube/shop/business/member/dto/MemberSessionDto.java
+++ src/main/java/com/slcube/shop/business/member/dto/MemberSessionDto.java
@@ -2,12 +2,10 @@ package com.slcube.shop.business.member.dto;
 
 import com.slcube.shop.business.member.domain.Member;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.io.Serializable;
 
 @Getter
-@Setter
 public class MemberSessionDto implements Serializable {
 
     private String email;
