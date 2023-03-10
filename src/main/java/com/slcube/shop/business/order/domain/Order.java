2c1f68c8c341217fb3d72b7c94cd32b9a68d12de
SLCube <kgm0096@naver.com>
2023-03-10 20:49:02 +0900

    refactor: Order, OrderItem 사이의 연관관계를 OneToMany, ManyToOne 양방향 연관관계로 변경

--- src/main/java/com/slcube/shop/business/order/domain/Order.java
+++ src/main/java/com/slcube/shop/business/order/domain/Order.java
@@ -1,6 +1,7 @@
 package com.slcube.shop.business.order.domain;
 
 import com.slcube.shop.business.delivery.domain.Delivery;
+import com.slcube.shop.business.order.dto.OrderCreateRequestDto;
 import com.slcube.shop.common.exception.OrderAlreadyCancelException;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -9,6 +10,8 @@ import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
@@ -33,11 +36,26 @@ public class Order {
 
     private Long memberId;
 
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
     @OneToOne(fetch = FetchType.LAZY, mappedBy = "order")
     private Delivery delivery;
 
-    protected Order(Long memberId) {
+    private Order(Long memberId) {
         this.memberId = memberId;
+    }
+
+    public static Order createOrder(Long memberId, List<OrderCreateRequestDto> requestDtoList) {
+        Order order = new Order(memberId);
+        requestDtoList.stream()
+                .forEach(requestDto -> {
+                    OrderItem orderItem = new OrderItem(requestDto.getItemId(), requestDto.getQuantity());
+                    order.getOrderItems().add(orderItem);
+                    orderItem.addOrder(order);
+                });
+
+        return order;
     }
 
     public void cancelOrder() {
--- src/main/java/com/slcube/shop/business/order/domain/OrderItem.java
+++ src/main/java/com/slcube/shop/business/order/domain/OrderItem.java
@@ -1,14 +1,10 @@
 package com.slcube.shop.business.order.domain;
 
-import com.slcube.shop.business.order.dto.OrderCreateRequestDto;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -20,7 +16,7 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
@@ -28,26 +24,12 @@ public class OrderItem {
 
     private int quantity;
 
-    @Builder
-    private OrderItem(Long itemId, int quantity) {
+    protected OrderItem(Long itemId, int quantity) {
         this.itemId = itemId;
         this.quantity = quantity;
     }
 
-    private void addOrder(Order order) {
+    protected void addOrder(Order order) {
         this.order = order;
-    }
-
-    public static List<OrderItem> createOrderItem(Long memberId, List<OrderCreateRequestDto> requestDtoList) {
-        Order order = new Order(memberId);
-
-        List<OrderItem> orderItems = requestDtoList.stream()
-                .map(requestDto -> {
-                    OrderItem orderItem = new OrderItem(requestDto.getItemId(), requestDto.getQuantity());
-                    orderItem.addOrder(order);
-                    return orderItem;
-                }).collect(Collectors.toList());
-
-        return orderItems;
     }
 }
